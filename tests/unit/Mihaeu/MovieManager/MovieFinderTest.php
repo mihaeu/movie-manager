9550d35d5c499673d9b95f175913928b93d28695
Michael Haeuslmann <haeuslmann@gmail.com>
2014-03-20 12:01:07 +0330

    Setup PHPUnit with config.

---
+++ tests/unit/Mihaeu/MovieManager/MovieFinderTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+
+/**
+ * Class FinderTest
+ *
+ * The Finder relies heavily on the filesystem, which is being mocked by vfs:://stream for testing purposes.
+ *
+ * @author Michael Haeuslmann <haeuslmann@gmail.com>
+ */
+class MovieFinderTest extends PHPUnit_Framework_TestCase
+{
+    public function testPHPUnitWorks()
+    {
+        $this->assertTrue(false);
+    }
+}
