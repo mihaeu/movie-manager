efa7a85f587aec73ce7ce9c5a15c63d12523cfea
Michael Haeuslmann <haeuslmann@gmail.com>
2014-06-12 11:27:45 +0200

    Started testing the Moviefinder.

--- src/Mihaeu/MovieManager/MovieFinder.php
+++ src/Mihaeu/MovieManager/MovieFinder.php
@@ -2,7 +2,56 @@
 
 namespace Mihaeu\MovieManager;
 
+use Mihaeu\Movie\Finder;
+
 class MovieFinder
 {
-    
+    public function find($rootMovieFolder)
+    {
+        $finder = new Finder($rootMovieFolder);
+        $movies = $finder->findMoviesInFolder();
+        // var_dump($movies);
+        
+        $movieInfo = [];
+        foreach ($movies as $movie) {
+            $movieFile = $movie->getMovieFilename();
+            $movieFolder = dirname($movieFile);
+            $movieInfo[] = [
+                'file'                      =>
+                    $movieFile,
+                
+                // Armageddon (1994).mp4 should be in a folder called Armageddon (1994)
+                'inProperMovieFolder'       =>
+                    $movieFolder === basename($movieFile, $movie->getMovieFileExtension()),
+                
+                // is the movie in a separate folder or a plain file under the root directory (because we want to have all the movies in a separate folder) (*)
+                'inSeparateFolderUnderRoot' =>
+                    dirname($movieFolder) === $rootMovieFolder,
+                
+                // does the movie have one or more subtitles
+                'hasSubtitle'               =>
+                    $movie->hasSubtitle(),
+                
+                // does the movie have a release info file (.nfo)
+                'hasNfoFile'                =>
+                    file_exists($movieFolder.DIRECTORY_SEPARATOR.$movie->getName().'.nfo'),
+                
+                // does the movie have a .url file and is it in our format with all the information (*)
+                
+                // does the movie have a poster (*)
+                
+                // what resolution is the movie in
+                
+                // what other files are in the movie folder
+
+            ];
+        }
+
+        return $movieInfo;
+    }
+
+    public function movieInMovieFolder($movieFilename, $rootMovieFolder)
+    {
+        return false;
+    }
 }
--- tests/unit/Mihaeu/MovieManager/MovieFinderTest.php
+++ tests/unit/Mihaeu/MovieManager/MovieFinderTest.php
@@ -3,18 +3,12 @@
 use org\bovigo\vfs\vfsStream;
 use org\bovigo\vfs\vfsStreamDirectory;
 
-/**
- * Class FinderTest
- *
- * The Finder relies heavily on the filesystem, which is being mocked by vfs:://stream for testing purposes.
- *
- * @author Michael Haeuslmann <haeuslmann@gmail.com>
- */
 class MovieFinderTest extends PHPUnit_Framework_TestCase
 {
     public function testPHPUnitWorks()
     {
         $movieFinder = new Mihaeu\MovieManager\MovieFinder();
-        $this->assertNotNull($movieFinder);
+        $this->assertEmpty($movieFinder->find(sys_get_temp_dir()));
+        // $this->assertEmpty($movieFinder->find('/home/mike/Desktop'));
     }
 }
