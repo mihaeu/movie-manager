dfafeb34ea7306c430fc38cc59af42e499a8d7af
Michael Haeuslmann <haeuslmann@gmail.com>
2014-10-06 19:12:21 +0200

    Create filestructure if it doesn't exist.

--- tests/unit/Tests/Sqlite3TestStorage.php
+++ tests/unit/Tests/Sqlite3TestStorage.php
@@ -8,7 +8,10 @@ class Sqlite3TestStorage extends Sqlite3Storage
 {
     public function __construct($fileName = '.phpunit-cas.db')
     {
-        $fileName = realpath(__DIR__.'/../../../'.$fileName);
+        $fileName = realpath(__DIR__.'/../../..').'/'.$fileName;
+        if (!file_exists(dirname($fileName))) {
+            mkdir(dirname($fileName), 0777, true);
+        }
         parent::__construct($fileName);
     }
 }
