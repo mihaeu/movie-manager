57f9ee802efd52bf057731171095d6b72436112e
Michael Haeuslmann <haeuslmann@gmail.com>
2014-10-12 17:41:06 +0200

    Testing IO.

---
+++ tests/unit/Tests/IO/FilesystemTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mihaeu\MovieManager\Tests\IO;
+
+class FilesystemTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSomething()
+    {
+
+    }
+}
---
+++ tests/unit/Tests/IO/IniTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Mihaeu\MovieManager\Tests\IO;
+
+use Mihaeu\MovieManager\IO\Filesystem;
+use Mihaeu\MovieManager\IO\Ini;
+use Mihaeu\MovieManager\Tests\BaseTestCase;
+
+class IniTest extends BaseTestCase
+{
+    public function tearDown()
+    {
+        $this->destroyTestStructure();
+    }
+
+    public function testWritesSimpleArray()
+    {
+        $ini = new Ini(new Filesystem());
+        $ini->write($this->testDirectory.'/test.ini', ['info' => ['test' => 'test']]);
+        $expected = "[info]\r\ntest=\"test\"\r\n\r\n";
+        $this->assertEquals($expected, file_get_contents($this->testDirectory.'/test.ini'));
+    }
+
+    public function testProperlyEscapesValues()
+    {
+
+    }
+
+    public function testWritesPlainString()
+    {
+        $ini = new Ini(new Filesystem());
+        $ini->write($this->testDirectory.'/test.ini', ['test']);
+        $expected = "0=\"test\"\r\n";
+        $this->assertEquals($expected, file_get_contents($this->testDirectory.'/test.ini'));
+    }
+
+    public function testWritesDeepArray()
+    {
+        $ini = new Ini(new Filesystem());
+        $ini->write($this->testDirectory.'/test.ini',
+            [
+                'info' => [
+                    'test' => [
+                        'test' => [
+                            1 => 'test']
+                    ]
+                ]
+            ]
+        );
+        $expected = "[info]\r\n"
+            ."test=\"test\"\r\n"
+            ."\r\n";
+        $this->assertEquals($expected, file_get_contents($this->testDirectory.'/test.ini'));
+    }
+}
